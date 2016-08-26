3cb77f1c670339d29ca363e826f61d5b88e07537
790036 <790036@seq.org>
2016-08-26 08:38:34 -0700

    Add files via upload

---
+++ FirstClassRunner.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class FirstClassRunner here.
+ * 
+ * @Levi Rutherford
+ * @FirstClass
+ */
+public class FirstClassRunner
+{
+   public static void main(String[] args){
+       FirstClass fc = new FirstClass();
+       System.out.printf("%n");
+       System.out.println("The sum of a and b is " + fc.addEmUp());
+       System.out.printf("%n");
+       System.out.println("The difference of a and b is " + fc.difference());
+    }
+}
